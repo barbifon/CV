36cf2c145b552290faf957a0a6bf7a1a0bd830dc
barbifon <bfontanella@estudiantes.unsam.edu.ar>
2023-01-19 14:19:38 -0300

    none

---
+++ index.js
@@ -0,0 +1,45 @@
+let infopersonal = document.getElementById("infopersonal")
+let experiencia = document.getElementById("experiencia")
+let educacion = document.getElementById("educacion")
+let intereses = document.getElementById("intereses")
+let carta = document.getElementById("carta")
+
+function mostrarInfoPersonal() {
+    infopersonal.style.display = "block"
+    experiencia.style.display = "none"
+    educacion.style.display = "none"
+    intereses.style.display = "none"
+    carta.style.display = "none"
+}
+
+function mostrarExperiencia() {
+    infopersonal.style.display = "none"
+    experiencia.style.display = "block"
+    educacion.style.display = "none"
+    intereses.style.display = "none"
+    carta.style.display = "none"
+}
+
+function mostrarEducacion() {
+    infopersonal.style.display = "none"
+    experiencia.style.display = "none"
+    educacion.style.display = "block"
+    intereses.style.display = "none"
+    carta.style.display = "none"
+}
+
+function mostrarIntereses() {
+    infopersonal.style.display = "none"
+    experiencia.style.display = "none"
+    educacion.style.display = "none"
+    intereses.style.display = "block"
+    carta.style.display = "none"
+}
+
+function mostrarCarta() {
+    infopersonal.style.display = "none"
+    experiencia.style.display = "none"
+    educacion.style.display = "none"
+    intereses.style.display = "none"
+    carta.style.display = "block"
+}
